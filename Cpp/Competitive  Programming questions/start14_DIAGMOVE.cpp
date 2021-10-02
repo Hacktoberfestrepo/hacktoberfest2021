a8f3e632f06a2112ef0aef4f0741c642b93c758c
kalp-77 <kalp07patel@gmail.com>
2021-10-02 22:38:38 +0530

    Create start14_DIAGMOVE.cpp

---
+++ Cpp/Competitive  Programming questions/start14_DIAGMOVE.cpp
@@ -0,0 +1,22 @@
+// codeshef DIV-3 start-14  
+// problem : DIAGMOVE
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long tc;
+    cin>>tc;
+    while(tc--){
+        int x,y;
+        cin>>x>>y;
+        if((x-y)%2 == 0){
+            cout<<"yes"<<endl;
+        }
+        else{
+            cout<<"no"<<endl;
+        }
+    
+    }
+    return 0;
+
+}
