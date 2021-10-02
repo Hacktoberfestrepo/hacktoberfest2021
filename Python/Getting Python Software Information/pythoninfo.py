1fc02e411df1c3edf6694344eeaccb8f51e49d42
Surjyanee Halder <75573569+surjyaneeh@users.noreply.github.com>
2021-10-02 12:08:55 +0530

    Add files via upload

---
+++ Python/Getting Python Software Information/pythoninfo.py
@@ -0,0 +1,6 @@
+import platform 
+print("My Python version is : ",platform.python_version())
+print("My Python version in tuple: ",platform.python_version_tuple())
+print("Build info of my Python software: ",platform.python_build())
+print("Compiler info of my Python Software: ",platform.python_compiler())
+print("Implementation: ",platform.python_implementation())
