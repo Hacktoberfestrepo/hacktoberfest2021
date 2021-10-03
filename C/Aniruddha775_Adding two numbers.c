c7f40877eab2391f1fdd292b1e6cfaf3050f351b
Aniruddha Dhar Chowdhury <91791624+Aniruddha775@users.noreply.github.com>
2021-10-03 23:40:25 +0530

    Aniruddha775 Added Two Numbers

---
+++ C/Aniruddha775_Adding two numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter 1st number \n");
+    scanf("%d", &a);
+    printf("Enter 2nd number \n");
+    scanf("%d", &b);
+    printf("The sum is %d\n", a+b);
+    return 0;
+    
+}
