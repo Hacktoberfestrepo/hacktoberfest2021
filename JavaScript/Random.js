4689b2966b78d847348027b05d46f7bf5272c652
Vishal Sharma <48996501+Str4nge@users.noreply.github.com>
2021-10-03 22:46:11 +0530

    Create Random.js

---
+++ JavaScript/Random.js
@@ -0,0 +1,9 @@
+// input from the user
+const min = parseInt(prompt("Enter a min value: "));
+const max = parseInt(prompt("Enter a max value: "));
+
+// generating a random number
+const a = Math.floor(Math.random() * (max - min + 1)) + min;
+
+// display a random number
+console.log(`Random value between ${min} and ${max} is ${a}`);
