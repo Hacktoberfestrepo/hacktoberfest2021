9ec09c946a0852474dc4afd8b25a289029650f5c
Aradhya Garg <78140726+Aradhyagarg@users.noreply.github.com>
2021-10-07 16:33:20 +0530

    Update postfixevaluation.cpp

--- Cpp/postfixevaluation.cpp
+++ Cpp/postfixevaluation.cpp
@@ -1,41 +1,30 @@
-class Solution
-{
-    public:
-    //Function to evaluate a postfix expression.
-    int evaluatePostfix(string s)
-    {
-        // Your code here
-        stack<int> st;
-        for(int i=0; i<s.length(); i++)
-        {
-            if(s[i]>='0' && s[i]<='9')
-            {
-                st.push(s[i]-'0');
-            }
-            else{
-                int op2=st.top();
-                st.pop();
-                int op1=st.top();
-                st.pop();
-                
-                switch (s[i])
-                {
-                    case '+':
-                    st.push(op1+op2);
-                    break;
-                    case '-':
-                    st.push(op1-op2);
-                    break;
-                    case '/':
-                    st.push(op1/op2);
-                    break;
-                    case '*':
-                    st.push(op1*op2);
-                    break;
-                    
-                }
-            }
-        }
-        return st.top();
-    }
-};
+#include<bits/stdc++.h>
+using namespace std;
+
+int evaluatePostfix(string exp){
+  stack<int> st;
+  for (int i = 0; exp[i]; ++i){
+    // If the scanned character is an operand (number here), push it to the stack.
+    if (isdigit(exp[i]))  
+      st.push(exp[i] - '0');  
+    // If the scanned character is an operator, pop two elements from stack apply the operator
+    else{  
+      int val2 = st.top();
+      st.pop();  
+      int val1 = st.top();
+      st.pop();  
+      switch (exp[i])  {  
+        case '+': st.push( val1 + val2); break;  
+        case '-': st.push( val1 - val2); break;  
+        case '*': st.push( val1 * val2); break;  
+        case '/': st.push( val1/val2); break;  
+      }  
+    }  
+  }  
+  return st.top(); 
+}
+
+int main(){
+  string exp = "632+7*+";
+  cout<<"Value of "<<exp<<" postfix expression is "<<evaluatePostfix(exp);
+}
