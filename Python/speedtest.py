f409964cc7b8feedf3b837234dbd16b6656fe1e0
kantagarg17 <53440019+kantagarg17@users.noreply.github.com>
2021-10-10 22:32:29 +0530

    Create speedtest.py

---
+++ Python/speedtest.py
@@ -0,0 +1,6 @@
+vimport speedtest
+speed = speedtest.Speedtest()
+download_speed = speed.download()
+upload_speed = speed.upload()
+print(f'The download speed is {download_speed}')
+print(f'The uplaod speed is {upload_speed}')                                          
