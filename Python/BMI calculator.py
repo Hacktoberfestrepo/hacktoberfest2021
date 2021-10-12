9bdf201d22da2f82033deafc0847c51c5938947c
Ahmed Rishad B A <56973548+Ahmed-Rishad-B-A@users.noreply.github.com>
2021-10-12 18:49:12 +0530

    update for diet

--- Python/BMI calculator.py
+++ Python/BMI calculator.py
@@ -5,12 +5,12 @@ BMI=Weight/(Height*Height)
 print("your Body Mass Index is: ",BMI)
 if(BMI>0):
 	if(BMI<=16):
-		print("you are severely underweight")
+		print("you are severely underweight, you have to eat more")
 	elif(BMI<=18.5):
-		print("you are underweight")
+		print("you are underweight try to eat spmemore")
 	elif(BMI<=25):
-		print("you are Healthy")
+		print("you are Healthyand keep it.")
 	elif(BMI<=30):
-		print("you are overweight")
+		print("you are overweight and try to do excercise")
 	else: print("you are severely overweight")
 else:("enter valid details")
