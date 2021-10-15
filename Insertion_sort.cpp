be68fb9043d354c06852d2d9dc7723b2b68f2cc8
SomRawat <76642632+SomRawat@users.noreply.github.com>
2021-10-15 12:59:02 +0530

    Insertion Sort
    Insertion Sort

---
+++ Insertion_sort.cpp
@@ -0,0 +1,18 @@
+// Insert an element from unsorted array to its correct position in sorted array
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    return 0;
+}
